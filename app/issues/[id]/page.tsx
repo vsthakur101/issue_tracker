6892a87fcb0bcc5184fb205a231239fe20108e6b
Vidhya <vsthakur.it0@gmail.com>
2024-06-18 22:25:01 +0530

    Disable SSR for loading markdown component

---
+++ app/issues/[id]/page.tsx
@@ -0,0 +1,34 @@
+import IssueStatusBadge from "@/app/components/IssueStatusBadge";
+import prisma from "@/prisma/client";
+import { Card, Flex, Heading, Text } from "@radix-ui/themes";
+import { notFound } from "next/navigation";
+import React from "react";
+import ReactMarkdown from "react-markdown";
+
+interface Props {
+  params: { id: string };
+}
+
+async function IssueDetails({ params }: Props) {
+  if (typeof +params.id !== "number") notFound();
+  const issue = await prisma.issue.findUnique({
+    where: {
+      id: parseInt(params?.id),
+    },
+  });
+  if (!issue) notFound();
+  return (
+    <div>
+      <Heading>{issue.title}</Heading>
+      <Flex className="space-x-3" my="2">
+        <IssueStatusBadge status={issue.status} />
+        <Text>{issue.createdAt.toDateString()}</Text>
+      </Flex>
+      <Card className="prose" mt="4">
+        <ReactMarkdown>{issue.description}</ReactMarkdown>
+      </Card>
+    </div>
+  );
+}
+
+export default IssueDetails;
---
+++ app/issues/new/loading.tsx
@@ -0,0 +1,14 @@
+import { Box } from "@radix-ui/themes";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+
+const NewIssueLoading = () => {
+  return (
+    <Box className="max-w-xl">
+      <Skeleton />
+      <Skeleton height="20rem" />
+    </Box>
+  );
+};
+
+export default NewIssueLoading;
